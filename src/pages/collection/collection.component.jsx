3148c3843cd93cd41acd9ce6894d3bdddd4e1dfe
Sergey Lem <sergey.lem@gmail.com>
2019-08-19 23:38:44 +0600

    changed components: collections, cart-icon with apollo

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
+    <div className='collection-page'> 
       <h2 className='title'>{title}</h2>
       <div className='items'>
         {items.map(item => (
@@ -21,8 +18,5 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
